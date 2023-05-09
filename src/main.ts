2e5cfbbfd427d924ca35310eca3b7b7fb5c4767e
radenlarko <radenlarko@gmail.com>
2023-05-09 14:22:01 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin: clientUrl,
+    origin: [clientUrl, 'http://192.168.0.102:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
   });
