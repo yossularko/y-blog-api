836d9bd3ab1c691acac46a667257d704493deeb8
radenlarko <radenlarko@gmail.com>
2023-05-10 09:03:35 +0700

    Update minor

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin: [clientUrl, 'http://192.168.0.102:3000'],
+    origin: [clientUrl, 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
   });
